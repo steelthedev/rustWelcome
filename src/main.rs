5a01c7a6aded4039fee82e9f218dbf5cb0355365
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-07-14 11:06:03 +0100

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,9 @@
+// Variables and Mutability
+
 fn main() {
-    let mut account: BankAccount = BankAccount { 
-        owner: "Alice".to_string(), 
-        balance: 2000.0 
-    };    
+    let mut _a: u16 = 5;
+    println!("{}", _a);
 
-    // Immutable borrow to check the balance 
-    account.check_balance();
-
-
-
-    //Mutable borrow to withdraw money
-    account.withdraw(50.0);
-
-    account.check_balance();
-}
-
-
-struct BankAccount{
-    owner: String,
-    balance: f64,
-}
-
-impl BankAccount {
-    fn withdraw(&mut self, amount: f64){
-        println!("withdrawing {} from account by {}", amount, self.owner);
-        self.balance -= amount;
-    }
-
-    fn check_balance(&self){
-        println!("Account owned by {} has a balance of {}", self.owner, self.balance);
-    }
-
+    _a = 67;
+    println!("{}", _a)
 }
