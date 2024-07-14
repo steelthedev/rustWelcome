1e75cb1dcf628f85f02f2287d29772a8b3403f1f
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-07-14 09:14:58 +0100

    primitive data type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+    //Integer
+
+    let x: i32 = -42;
+    let y: u64 = 65;
+    println!("Signed Integer:{}", x);
+    println!("Unsigned Integer: {}", y);
+
+    //Floats
+    let pi: f32 = 3.14;
+    println!("Value of pi: {}", pi);
+
+    //Boolean
+
+    let is_snwoing: bool = true;
+    println!("Is it snowing?: {}", is_snwoing);
+
+    // Character type
+
+    let letter: char = 'a';
+    println!("First letter of the alphabet is: {}",letter);
 }
