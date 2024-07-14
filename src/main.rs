4da4031f64d62f6c5df9175f62284ac577423d0e
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-07-15 00:10:10 +0100

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,39 @@
+use std::net::IpAddr;
 
 
 fn main() {
-
-    let user: User = build_new_user(
-        String::from("akinwumikaliyanu@gmail.com"), 
-        String::from("steelthedev"), 
-        true,
-        5);
-
-    let mut book: Book = build_new_book(
-        String::from("Struct in rust"),
-        user,
-        56,
-        true);
-
-    book.print_book_name();
-    book.alter_book_name(String::from("Altered Carbon"));    
-    book.print_author_details();
-}
-
-
-struct Book{
-    title: String,
-    author: User,
-    pages: u32,
-    available: bool,
-}
-
-#[derive(Debug)]
-struct User{
-    active: bool,
-    username: String,
-    email: String, 
-    sign_in_count: u64,
-}
-
-
-fn build_new_user(email: String, username: String, active: bool, sign_in_count: u64) -> User{
-    User { active: active, username: username, email: email, sign_in_count: sign_in_count }
-}
-
-fn build_new_book(title: String, author: User, pages:u32, available: bool) -> Book{
-    Book { title: title, author: author, pages: pages, available: available }
-}
-
-
-impl Book {
-    fn print_book_name(&self){
-        println!("Book name is {}", self.title)
+    enum IpAddr {
+        V4(u8,u8,u8,u8),
+        V6(String),
     }
 
-    fn alter_book_name(&mut self, new_book_name:String){
-        self.title = new_book_name;
-        self.print_book_name()
-    }
 
-    fn print_author_details(&self){
-        println!("Author's details, {:?}", self.author.print_user_details());
-    }
-}
+    // let _home = IpAddr::V4(String::from("127.0.0.1"));
+    // let _loopback = IpAddr::V6(String::from("::1"));
 
-impl User{
-    fn print_user_details(&self){
-        println!("{:?}", self);
-    }
+
+    // ENhanced enums
+
+
+    let _home = IpAddr::V4(127,0,0,1);
+    let _loopback = IpAddr::V6(String::from("::1"));
+
+
+    // struct  IpAddr{
+    //     kind: IpAddrKind,
+    //     address: String
+    // }
+
+    // let _four = IpAddrKind::V4;
+    // let _six = IpAddrKind::V6;
+
+    // let _home: IpAddr = IpAddr {
+    //      kind: _four,
+    //      address: String::from("127.0.0.1") 
+    //     };
+
+    // let _loopback: IpAddr = IpAddr { kind: _six, address: String::from("::1") };
+   
+
+   
 }
