8c94270c5241acc194ce1188ee247336a177c66b
Akinwumi Iyanuoluwa Ayomiposi <akinwumikaliyanu@gmail.com>
2024-07-14 23:03:03 +0100

    IfElseStatement

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+
 fn main() {
-    let x = 5;
+//  let age: u16 = 18;
 
-    let x = x + 1;
-    
-    {
-        let x = x*2;
-        println!("{}", x)
-    }
+//  if age >= 18{
+//     println!("You are old enough to code");
+//  }else {
+//      println!("You can't code");
+//  }
+ 
+// Multiple conditions with else if
+ let number: i32 = 100;
 
-    println!("{}", x)
+ if number % 4 == 0{
+    println!("Number is divisible by 4");
+ }else if number % 3 == 0 {
+    println!("Number is divisible by 3");
+ }else if number % 2 == 0 {
+    println!("Number is divisible by 2");
+ }else {
+     println!("Number not divisible by 4, 3, or 2");
+ }
+
+ // If in a let statement
+
+ let condition = true;
+ let number = if condition {5} else {6};
+ println!("{}", number)
 }
